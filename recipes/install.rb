5f6dbb5e07e338f9156447cd3dfb4acb80a11100
interu <beautybeast07@gmail.com>
2016-05-20 14:12:07 +0900

    rpmを含むように

--- recipes/install.rb
+++ recipes/install.rb
@@ -1,10 +1,19 @@
 install_config = node['remote_syslog2']['install']
 bin_file = 'remote_syslog'
 
-remote_file install_config['download_path'] do
-  not_if "ls /usr/local/bin/#{bin_file}"
-  source install_config['source_url']
-  retries 2
+## githubによってアクセス拒否されている可能性あるので
+#remote_file install_config['download_path'] do
+#  not_if "ls /usr/local/bin/#{bin_file}"
+#  source install_config['source_url']
+#  retries 2
+#end
+
+cookbook_file install_config['download_path'] do
+  source 'remote_syslog2-0.18-1.x86_64.rpm'
+  mode 755
+  owner 'root'
+  group 'root'
+  action :create
 end
 
 package bin_file do
