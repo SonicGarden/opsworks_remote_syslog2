3f82dabc200736915b5ee9fc764028bfae5f7f65
interu <beautybeast07@gmail.com>
2016-05-20 13:08:13 +0900

    fix miss type

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,4 @@
-name             "remote_syslog2"
+name             "opsworks_remote_syslog2"
 maintainer       "interu"
 maintainer_email "interu@sonicgarden.jp"
 license          "Apache 2.0"
@@ -6,7 +6,7 @@ description      "Installs/Configures remote_syslog2"
 version          "0.1"
 
 
-recipe 'remote_syslog2', 'Installs/Configures remote_syslog2'
-recipe 'remote_syslog2::configure', 'Generates config file for remote_syslog2'
-recipe 'remote_syslog2::install', 'Installs remote_syslog2'
-recipe 'remote_syslog2::service', ' Installs remote_syslog2 as an init.d service and starts/enables it'
+recipe 'opsworks_remote_syslog2', 'Installs/Configures remote_syslog2'
+recipe 'opsworks_remote_syslog2::configure', 'Generates config file for remote_syslog2'
+recipe 'opsworks_remote_syslog2::install', 'Installs remote_syslog2'
+recipe 'opsworks_remote_syslog2::service', ' Installs remote_syslog2 as an init.d service and starts/enables it'
