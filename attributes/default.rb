7ceaad7eb7895b4a8cce64079dcefee0e81712eb
interu <beautybeast07@gmail.com>
2016-05-20 13:48:28 +0900

    rpmダウンロードをhttpで実施るように

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,6 +8,6 @@ default['remote_syslog2']['config'] = {
 }
 
 # These attributes probably shouldn't be changed unless they specifically need to be
-default['remote_syslog2']['install']['source_url'] = 'https://github.com/papertrail/remote_syslog2/releases/download/v0.18-beta1/remote_syslog2-0.18-1.x86_64.rpm'
+default['remote_syslog2']['install']['source_url'] = 'http://github.com/papertrail/remote_syslog2/releases/download/v0.18-beta1/remote_syslog2-0.18-1.x86_64.rpm'
 default['remote_syslog2']['install']['download_path'] = '/tmp/remote_syslog2.x86_64.rpm'
 default['remote_syslog2']['config_file'] = '/etc/log_files.yml'
