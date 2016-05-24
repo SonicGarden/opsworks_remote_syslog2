0b42742daf8cf7de7940023e2d7dadd625802237
interu <beautybeast07@gmail.com>
2016-05-24 10:57:34 +0900

    deploy後にremote_syslogの再起動が行われるように

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -6,16 +6,3 @@ cookbook_file '/etc/init.d/remote_syslog' do
   group 'root'
   action :create
 end
-
-template node['remote_syslog2']['install']['config_file'] do
-  source 'log_files.yml.erb'
-  mode 0644
-  owner 'root'
-  group 'root'
-  notifies :restart, "service[remote_syslog]", :immediately
-end
-
-service 'remote_syslog' do
-  supports status: true, restart: true, reload: true
-  action :nothing
-end
--- recipes/service.rb
+++ recipes/service.rb
@@ -1,3 +1,11 @@
+template node['remote_syslog2']['install']['config_file'] do
+  source 'log_files.yml.erb'
+  mode 0644
+  owner 'root'
+  group 'root'
+  notifies :restart, "service[remote_syslog]", :immediately
+end
+
 service 'remote_syslog' do
   supports status: true, restart: true, reload: true
   action :nothing
